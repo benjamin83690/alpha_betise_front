a101cb42c50fb02e30fb04d589dab94412ad7e63
Arnaud Costard <arnaud_costard@hotmail.fr>
2023-01-12 11:04:20 +0100

    Création components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,11 @@ import { CommentaireComponent } from './components/commentaire/commentaire.compo
 import { AccueilComponent } from './components/accueil/accueil.component';
 import { RencontresComponent } from './components/rencontres/rencontres.component';
 import { LibrairieComponent } from './components/librairie/librairie.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { PanierComponent } from './components/panier/panier.component';
+import { ProduitComponent } from './components/produit/produit.component';
+import { InscriptionEvenementComponent } from './components/inscription-evenement/inscription-evenement.component';
+import { ConseilsLectureComponent } from './components/conseils-lecture/conseils-lecture.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +25,12 @@ import { LibrairieComponent } from './components/librairie/librairie.component';
     CommentaireComponent,
     AccueilComponent,
     RencontresComponent,
-    LibrairieComponent
+    LibrairieComponent,
+    FooterComponent,
+    PanierComponent,
+    ProduitComponent,
+    InscriptionEvenementComponent,
+    ConseilsLectureComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/conseils-lecture/conseils-lecture.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-conseils-lecture',
+  templateUrl: './conseils-lecture.component.html',
+  styleUrls: ['./conseils-lecture.component.css']
+})
+export class ConseilsLectureComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
