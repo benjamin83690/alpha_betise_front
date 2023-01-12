d6a44221eadb67bcfdab6b499c6392e04b10b25b
Arnaud Costard <arnaud_costard@hotmail.fr>
2023-01-12 11:04:20 +0100

    Création components

---
+++ src/app/components/inscription-evenement/inscription-evenement.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inscription-evenement',
+  templateUrl: './inscription-evenement.component.html',
+  styleUrls: ['./inscription-evenement.component.css']
+})
+export class InscriptionEvenementComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
