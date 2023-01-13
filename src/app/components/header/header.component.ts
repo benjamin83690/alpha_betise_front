073e49008e29e32df7243b7d1ba575fbc823e255
Benjamin Fichot <fichot.benjamin@gmail.com>
2023-01-13 14:46:31 +0100

    15-panier-component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  opened!: boolean;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  valueChanged(bool: any) {
+    this.opened = bool;
   }
 
 }
--- src/app/components/panier/panier.component.ts
+++ src/app/components/panier/panier.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild, OnChanges, SimpleChanges, Output, EventEmitter } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-panier',
   templateUrl: './panier.component.html',
   styleUrls: ['./panier.component.css']
 })
-export class PanierComponent implements OnInit {
+export class PanierComponent implements OnInit, OnChanges {
+
+  @ViewChild('sidenav') sidenav!: MatSidenav;
+  @Input() isExpanded!: boolean;
+  @Output() toggleValue = new EventEmitter<boolean>();
+  @Input() detailsCommande: any = {};
+  prixByQuantity: number = 0;
 
   constructor() { }
-
+  
+  ngOnChanges(changes: SimpleChanges): void {
+    this.isExpanded = changes['isExpanded'].currentValue;
+  }
+  
   ngOnInit(): void {
+  }
+
+  closeNav() {
+    this.sidenav.close();
+    this.isExpanded = false;
+    this.toggleValue.emit(this.isExpanded);
+  }
+
+  total() {
+    return (this.detailsCommande.map((detail:any) => detail.prix * detail.quantite).reduce((acc:any, val:any) => acc + val));
   }
 
 }
