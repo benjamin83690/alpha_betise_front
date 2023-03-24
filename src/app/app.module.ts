9012c271e7d8edd8c9627589088995df3d333884
Benjamin Fichot <fichot.benjamin@gmail.com>
2023-03-24 18:15:27 +0100

    48-refonte-package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,12 @@
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BackOfficeModule } from './back-office/backOfficeModule/back-office.module';
+import { PublicModule } from './public/publicModule/public.module';
+import { SharedModule } from './shared/sharedModule/shared.module';
 import { TokenInterceptor } from './shared/tokenInterceptor/token.interceptor';
-import { SharedModule } from './shared/sharedModule/shared/shared.module';
-import { PublicModule } from './public/publicModule/public/public.module';
-import { BackOfficeModule } from './back-office/backOfficeModule/back-office/back-office.module';
 
 @NgModule({
   declarations: [
